8c8a1f47efde6fd8a24e78bc6628d447362c3b06
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-22 23:19:38 +0300

    setup.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import viteLogo from '/vite.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello Vite</h1>
+    </div>
+  );
+}
+
+export default App;
