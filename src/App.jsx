04ef673f5ca457cbd5d81169991ee41a818128a6
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-23 10:56:21 +0300

    build the scaletal of the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Header from './components/Header';
+import Main from './components/Main';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/5</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
