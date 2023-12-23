e27d8a72fde5a0b5cd0d7c36b16bca1f184ab4ef
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-23 11:18:14 +0300

    install json-server and build the app.jsx file to fetch the questions and put them on the usereducer state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useReducer } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: 'loading',
+};
+
+function reducer(currState, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return { ...currState, questions: action.payload, status: 'ready' };
+      break;
+    case 'dataFailed':
+      return { ...currState, status: 'error' };
+      break;
+    case '':
+      return { ...currState, questions: action.payload, status: 'ready' };
+      break;
+
+    default:
+      throw new Error('Unkown Action');
+      break;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Fetch data
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataRecieved', payload: data }))
+      .catch(() => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 function Main({ children }) {
   return <main className="main">{children}</main>;
 }
